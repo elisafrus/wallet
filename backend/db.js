c78fe3be99d0f2c6a519285a8569c64d4a7b93f8
Lisa <lisafrusenko@icloud.com>
2025-12-16 02:35:13 +0200

    Fix: Умовне підключення до БД для обходу ECONNREFUSED у CI

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,28 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'qwerty11',
-  database: 'wallet',
-});
+let connection = null;
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL Database!');
-});
+if (process.env.NODE_ENV !== 'test' && !process.env.CI) {
+  connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'qwerty11',
+    database: 'wallet',
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL Database:', err.message);
+      throw err;
+    }
+    console.log('Connected to MySQL Database!');
+  });
+} else {
+  console.log('Skipping MySQL connection in CI/Test environment.');
+  connection = {
+    query: (sql, callback) => callback(new Error('DB connection is stubbed in test mode')),
+    end: () => {}
+  };
+}
 
 module.exports = connection;
